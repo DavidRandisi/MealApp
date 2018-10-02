ab59bdbfcb040b095235bd84a770843eeae8e052
DavidRandisi <bydive@hotmail.com>
2018-10-02 18:16:48 +0200

    Added gson library dependency and created MealFactory to store parsed meal objects

---
+++ app/src/main/java/student/pxl/be/mealapp/meals/MealFactory.java
@@ -0,0 +1,13 @@
+package student.pxl.be.mealapp.meals;
+
+public class MealFactory {
+    public Meal[] meals;
+
+    public Meal[] getMeals() {
+        return meals;
+    }
+
+    public void setMeals(Meal[] meals) {
+        this.meals = meals;
+    }
+}
