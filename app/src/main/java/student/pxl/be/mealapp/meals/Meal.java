9d86999f68e327c6e30cd4ba69d59aec80a13125
DavidRandisi <bydive@hotmail.com>
2018-10-02 18:46:18 +0200

    Modified Meal model class to map json values to property values

--- app/src/main/java/student/pxl/be/mealapp/meals/Meal.java
+++ app/src/main/java/student/pxl/be/mealapp/meals/Meal.java
@@ -1,114 +1,97 @@
 package student.pxl.be.mealapp.meals;
 
-import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Map;
 
 public class Meal {
-    //extends data.ObjectWithId implements Parcelable
-    public int id;
-    public int name;
-    public String categroy;
-    public String instructions;
-    public String image;
-    public String tags;
-    public String youtube;
-    public List<String> Ingredients;
-    public List<String> measures;
-    public boolean favorite;
+    @SerializedName("idMeal")
+    private int id;
+    @SerializedName("strMeal")
+    private String name;
+    @SerializedName("strCategory")
+    private String category;
+    @SerializedName("strArea")
+    private String area;
+    @SerializedName("strInstruction")
+    private String description;
+    @SerializedName("strMealThumb")
+    private String pictureLink;
+    @SerializedName("strTags")
+    private String tags;
+    @SerializedName("strYoutube")
+    private String youtubeLink;
+    @SerializedName("strSource")
+    private String source;
 
-    public Meal(int id, int name, String categroy, String instructions, String image, String tags, String youtube, List<String> ingredients, List<String> measures, boolean favorite) {
-        this.id = id;
-        this.name = name;
-        this.categroy = categroy;
-        this.instructions = instructions;
-        this.image = image;
-        this.tags = tags;
-        this.youtube = youtube;
-        Ingredients = ingredients;
-        this.measures = measures;
-        this.favorite = favorite;
-    }
+    //Ingredients
+    private String strIngredient1;
+    private String strIngredient2;
+    private String strIngredient3;
+    private String strIngredient4;
+    private String strIngredient5;
+    private String strIngredient6;
+    private String strIngredient7;
+    private String strIngredient8;
+    private String strIngredient9;
+    private String strIngredient10;
+    private String strIngredient11;
+    private String strIngredient12;
+    private String strIngredient13;
+    private String strIngredient14;
+    private String strIngredient15;
+    private String strIngredient16;
+    private String strIngredient17;
+    private String strIngredient18;
+    private String strIngredient19;
+    private String strIngredient20;
 
-    public Meal() {
+    //Measurements
+    private String strMeasure1;
+    private String strMeasure2;
+    private String strMeasure3;
+    private String strMeasure4;
+    private String strMeasure5;
+    private String strMeasure6;
+    private String strMeasure7;
+    private String strMeasure8;
+    private String strMeasure9;
+    private String strMeasure10;
+    private String strMeasure11;
+    private String strMeasure12;
+    private String strMeasure13;
+    private String strMeasure14;
+    private String strMeasure15;
+    private String strMeasure16;
+    private String strMeasure17;
+    private String strMeasure18;
+    private String strMeasure19;
+    private String strMeasure20;
 
-    }
+    //Key:Ingredient - Value:Measurement
+    private Map<String,String> ingredientsWithMeasurements;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getName() {
-        return name;
-    }
-
-    public void setName(int name) {
-        this.name = name;
-    }
-
-    public String getCategroy() {
-        return categroy;
-    }
-
-    public void setCategroy(String categroy) {
-        this.categroy = categroy;
-    }
-
-    public String getInstructions() {
-        return instructions;
-    }
-
-    public void setInstructions(String instructions) {
-        this.instructions = instructions;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getTags() {
-        return tags;
-    }
-
-    public void setTags(String tags) {
-        this.tags = tags;
-    }
-
-    public String getYoutube() {
-        return youtube;
-    }
-
-    public void setYoutube(String youtube) {
-        this.youtube = youtube;
-    }
-
-    public List<String> getIngredients() {
-        return Ingredients;
-    }
-
-    public void setIngredients(List<String> ingredients) {
-        Ingredients = ingredients;
-    }
-
-    public List<String> getMeasures() {
-        return measures;
-    }
-
-    public void setMeasures(List<String> measures) {
-        this.measures = measures;
-    }
-
-    public boolean isFavorite() {
-        return favorite;
-    }
-
-    public void setFavorite(boolean favorite) {
-        this.favorite = favorite;
+    public Map<String, String> getIngredientsWithMeasurements() {
+        ingredientsWithMeasurements.put(strIngredient1, strMeasure1);
+        ingredientsWithMeasurements.put(strIngredient2, strMeasure2);
+        ingredientsWithMeasurements.put(strIngredient3, strMeasure3);
+        ingredientsWithMeasurements.put(strIngredient4, strMeasure4);
+        ingredientsWithMeasurements.put(strIngredient5, strMeasure5);
+        ingredientsWithMeasurements.put(strIngredient6, strMeasure6);
+        ingredientsWithMeasurements.put(strIngredient7, strMeasure7);
+        ingredientsWithMeasurements.put(strIngredient8, strMeasure8);
+        ingredientsWithMeasurements.put(strIngredient9, strMeasure9);
+        ingredientsWithMeasurements.put(strIngredient10, strMeasure10);
+        ingredientsWithMeasurements.put(strIngredient11, strMeasure11);
+        ingredientsWithMeasurements.put(strIngredient12, strMeasure12);
+        ingredientsWithMeasurements.put(strIngredient13, strMeasure13);
+        ingredientsWithMeasurements.put(strIngredient14, strMeasure14);
+        ingredientsWithMeasurements.put(strIngredient15, strMeasure15);
+        ingredientsWithMeasurements.put(strIngredient16, strMeasure16);
+        ingredientsWithMeasurements.put(strIngredient17, strMeasure17);
+        ingredientsWithMeasurements.put(strIngredient18, strMeasure18);
+        ingredientsWithMeasurements.put(strIngredient19, strMeasure19);
+        ingredientsWithMeasurements.put(strIngredient20, strMeasure20);
+        return ingredientsWithMeasurements;
     }
 }
