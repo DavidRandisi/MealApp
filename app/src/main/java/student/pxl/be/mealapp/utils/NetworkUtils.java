6ea3ebd74599baeb79d54f525b90892a527c8e8f
DavidRandisi <bydive@hotmail.com>
2018-10-02 16:26:00 +0200

    Added NetworkUtils helper method to make a HTTP request and return data in a JSON String

--- app/src/main/java/student/pxl/be/mealapp/utils/NetworkUtils.java
+++ app/src/main/java/student/pxl/be/mealapp/utils/NetworkUtils.java
@@ -2,8 +2,12 @@ package student.pxl.be.mealapp.utils;
 
 import android.net.Uri;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
  * Utility class that will be used to communicate over the network with the MealAPI
@@ -36,5 +40,23 @@ public class NetworkUtils {
             exception.printStackTrace();
         }
         return url;
+    }
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
