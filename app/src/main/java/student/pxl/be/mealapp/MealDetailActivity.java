df7f83cc788a5a3388641e4ac27e194dfa11e183
DavidRandisi <david_randisi@hotmail.com>
2018-10-14 18:27:08 +0200

    Created MealDetailActivity with a layout that creates and adds a fragment with the given meal argument

---
+++ app/src/main/java/student/pxl/be/mealapp/MealDetailActivity.java
@@ -0,0 +1,47 @@
+package student.pxl.be.mealapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import student.pxl.be.mealapp.domain.Meal;
+import student.pxl.be.mealapp.fragments.MealDetailFragment;
+
+public class MealDetailActivity extends AppCompatActivity {
+    private static final String TAG = "MealDetailActivity";
+    private Meal meal;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mealdetail);
+        Log.d(TAG, "onCreate: started");
+
+        getIncomingIntent();
+
+        //Create fragment with the retrieved meal from the intent
+        Bundle args = new Bundle();
+        args.putParcelable("clickedMeal", meal);
+        Fragment mealDetailFragment = new MealDetailFragment();
+        mealDetailFragment.setArguments(args);
+
+        //Add fragment to the parent layout that corresponds to this activity
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.meal_detail_parent_layout_id, mealDetailFragment);
+        fragmentTransaction.commit();
+    }
+
+    //Read arguments from the incoming intent if they exist
+    private void getIncomingIntent(){
+        Log.d(TAG, "getIncomingIntent: Checking for incoming Intent");
+        if(getIntent().hasExtra("clickedMeal")){
+            Log.d(TAG, "getIncomingIntent: found intent extras");
+            meal = getIntent().getParcelableExtra("clickedMeal");
+        }
+    }
+}
