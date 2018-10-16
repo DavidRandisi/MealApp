fd754ed11839eafd8e67a83e0a080e4293580e02
Fatih <fatihb@live.be>
2018-10-16 14:15:59 +0200

    Created DB for (user-added) Local Meals
    DB will be loaded for user added meals

---
+++ app/src/main/java/student/pxl/be/mealapp/data/LocalMealsDB.java
@@ -0,0 +1,35 @@
+package student.pxl.be.mealapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class LocalMealsDB extends SQLiteOpenHelper{
+
+    private static final String DATABASE_NAME = "Meals";
+
+    public LocalMealsDB(Context context){
+       super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_LOCALMEALS_TABLE = "CREATE TABLE LOCALMEALS(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "title TEXT," +
+                "ingredients TEXT," +
+                "thumbnail TEXT)"; //TODO: thumbnail from camera
+        db.execSQL(CREATE_LOCALMEALS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Drop older table if exists
+        db.execSQL("DROP TABLE IF EXISTS LOCALMEALS");
+
+        //create fresh table
+        this.onCreate(db);
+    }
+
+    
+}
