f3c71394c4fba2b7820b505ba65dc2c5414aad77
Fatih <fatihb@live.be>
2018-10-18 16:45:26 +0200

    Added MealController and function to add meals

---
+++ app/src/main/java/student/pxl/be/mealapp/domain/MealController.java
@@ -0,0 +1,22 @@
+package student.pxl.be.mealapp.domain;
+
+import android.content.Context;
+
+import student.pxl.be.mealapp.data.AsyncDatabaseHandler;
+
+public class MealController {
+
+    private final Context appContext;
+    private final AsyncDatabaseHandler asyncDatabaseHandler;
+
+    public MealController(Context context){
+        appContext = context.getApplicationContext();
+        asyncDatabaseHandler = new AsyncDatabaseHandler() { //TODO: not sure why we have to initialize this
+        };
+    }
+
+    public void addMeal(Meal meal) {
+        asyncDatabaseHandler.asyncInsert(meal);
+    }
+
+}
