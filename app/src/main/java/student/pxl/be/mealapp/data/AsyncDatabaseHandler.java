f2d731e310834e706847beccb6f4f5cf1d0869ac
Fatih <fatihb@live.be>
2018-10-18 16:30:03 +0200

    Added async task for new local meals

--- app/src/main/java/student/pxl/be/mealapp/data/AsyncDatabaseHandler.java
+++ app/src/main/java/student/pxl/be/mealapp/data/AsyncDatabaseHandler.java
@@ -1,6 +1,22 @@
 package student.pxl.be.mealapp.data;
 
-public class AsyncDatabaseHandler {
+import android.content.Context;
+import android.os.AsyncTask;
+import student.pxl.be.mealapp.domain.Meal;
+
+public abstract class AsyncDatabaseHandler {
+
+    private LocalMealsTableManager tabelManager;
+
+    public final void asyncInsert(Meal meal) {
+        new AsyncTask<Void, Void, Integer>(){
+            @Override
+            protected Integer doInBackground(Void... params){
+                tabelManager.addMeal(meal);
+                return meal.getId();
+            }
+        }.execute();
+    }
 
     //TODO: Add Async tasks
 }
