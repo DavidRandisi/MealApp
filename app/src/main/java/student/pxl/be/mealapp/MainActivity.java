552d45a5a907b17c78ce3800f91e596224346439
DavidRandisi <david_randisi@hotmail.com>
2018-10-20 23:22:35 +0200

    Display master and detail fragment in landscape mode

--- app/src/main/java/student/pxl/be/mealapp/MainActivity.java
+++ app/src/main/java/student/pxl/be/mealapp/MainActivity.java
@@ -3,41 +3,67 @@ package student.pxl.be.mealapp;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import student.pxl.be.mealapp.domain.Meal;
 import student.pxl.be.mealapp.fragments.MealsFragment;
 
 public class MainActivity extends AppCompatActivity {
-    private TabLayout mtabLayout;
-    private ViewPager mviewPager;
     private static final String MEALS__KEY = "mealkey";
     private ArrayList<Meal> randomMeals;
     private ArrayList<Meal> favoriteMeals;
     private ArrayList<Meal> localMeals;
+    private boolean isTwoPane;
+    private ViewPager mviewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        determineTwoPane();
 
-        mtabLayout = findViewById(R.id.tabs);
-        mviewPager = findViewById(R.id.vp_id);
         randomMeals = new ArrayList<>();
         favoriteMeals = new ArrayList<>();
         localMeals = new ArrayList<>();
+        mviewPager = findViewById(R.id.vp_id);
 
-        FragmentPagerAdapter viewPagerAdapter = new ViewPagerAdapter();
-        mviewPager.setAdapter(viewPagerAdapter);
-        mtabLayout.setupWithViewPager(mviewPager);
+        //TODO: add navigation in TwoPane (landscape) mode between explore/favorites/local
+        if(isTwoPane){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            //TODO: save CURRENT viewpager fragment instance to display it in landscape mode
+            fragmentTransaction.replace(R.id.land_list_frame_id, getExploreMealsFragment());
+            fragmentTransaction.commit();
+        } else {
+            TabLayout mtabLayout = findViewById(R.id.tabs);
+            ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter();
+            mviewPager.setAdapter(viewPagerAdapter);
+            mtabLayout.setupWithViewPager(mviewPager);
+        }
+    }
 
+    //Decides if the screen is in twoPane mode by checking if the meal detail frame is available
+    private void determineTwoPane(){
+        if(findViewById(R.id.land_detail_frame_id) != null){
+            isTwoPane = true;
+        } else {
+            isTwoPane = false;
+        }
     }
 
     private class ViewPagerAdapter extends FragmentPagerAdapter {
@@ -49,9 +75,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public Fragment getItem(int position) {
             switch (position) {
-                case 0: return fetchExploreMealsDataAndCreateFragment();
-                case 1: return fetchFavoriteMealsDataAndCreateFragment();
-                case 2: return fetchLocalMealsDataAndCreateFragment();
+                case 0: return getExploreMealsFragment();
+                case 1: return getFavoriteMealsFragment();
+                case 2: return getLocalMealsFragment();
 
                 default: throw new IllegalArgumentException("unexpected position: " + position);
             }
@@ -76,54 +102,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //TODO: replace dummy data with data from a AsyncTaskLoader API call
-    private MealsFragment fetchExploreMealsDataAndCreateFragment() {
-        for(int i = 0; i<20; i++){
+    //TODO: save fragment state so the API call doesn't get executed on every screen rotation
+    private MealsFragment getExploreMealsFragment() {
+        if(randomMeals.size() == 0){
+            for(int i = 0; i<20; i++){
+                Meal meal = new Meal();
+                meal.thumbnail = "http://img.recipepuppy.com/11.jpg";
+                meal.title = "TO BE REPLACED WITH API MEALS";
+                meal.ingredients = "Dummy Chicken, Dummy Rice";
+                meal.href = "https://www.allrecipes.com/recipe/14746/mushroom-pork-chops/";
+                randomMeals.add(meal);
+            }
             Meal meal = new Meal();
-            meal.thumbnail = "http://img.recipepuppy.com/11.jpg";
-            meal.title = "TO BE REPLACED WITH API MEALS";
-            meal.ingredients = "Dummy Chicken, Dummy Rice";
+            meal.thumbnail = "http://img.recipepuppy.com/10.jpg";
+            meal.title = "Different meal to test navigation";
+            meal.ingredients = "Dummy Crisps, Dummy Ketchup";
             meal.href = "https://www.allrecipes.com/recipe/14746/mushroom-pork-chops/";
             randomMeals.add(meal);
         }
+
         //Create argument bundle that contains the meal list to display in the particular recyclerview
         Bundle args = new Bundle();
         args.putParcelableArrayList(MEALS__KEY, randomMeals);
+        args.putBoolean("isTwoPane", isTwoPane);
         MealsFragment exploreListFragment = new MealsFragment();
         exploreListFragment.setArguments(args);
         return exploreListFragment;
     }
 
-    private MealsFragment fetchFavoriteMealsDataAndCreateFragment(){
-        for(int i = 0; i<20; i++){
-            Meal meal = new Meal();
-            meal.thumbnail = "http://img.recipepuppy.com/12.jpg";
-            meal.title = "TO BE REPLACED WITH SQLITE FAVORITE MEALS";
-            meal.ingredients = "Dummy Chicken, Dummy Rice";
-            meal.href = "https://www.allrecipes.com/recipe/14746/mushroom-pork-chops/";
-            favoriteMeals.add(meal);
+    private MealsFragment getFavoriteMealsFragment(){
+        if(favoriteMeals.size() == 0){
+            for(int i = 0; i<20; i++){
+                Meal meal = new Meal();
+                meal.thumbnail = "http://img.recipepuppy.com/12.jpg";
+                meal.title = "TO BE REPLACED WITH SQLITE FAVORITE MEALS";
+                meal.ingredients = "Dummy Chicken, Dummy Rice";
+                meal.href = "https://www.allrecipes.com/recipe/14746/mushroom-pork-chops/";
+                favoriteMeals.add(meal);
+            }
         }
-
         //Create argument bundle that contains the meal list to display in the particular recyclerview
         Bundle args = new Bundle();
         args.putParcelableArrayList(MEALS__KEY, favoriteMeals);
+        args.putBoolean("isTwoPane", isTwoPane);
         MealsFragment favoriteListFragment = new MealsFragment();
         favoriteListFragment.setArguments(args);
         return favoriteListFragment;
     }
 
-    private MealsFragment fetchLocalMealsDataAndCreateFragment(){
-        for(int i = 0; i<20; i++){
-            Meal meal = new Meal();
-            meal.thumbnail = "http://img.recipepuppy.com/16.jpg";
-            meal.title = "TO BE REPLACED WITH SQLITE LOCAL MEALS";
-            meal.ingredients = "Dummy Chicken, Dummy Rice";
-            meal.href = "https://www.allrecipes.com/recipe/14746/mushroom-pork-chops/";
-            localMeals.add(meal);
+    private MealsFragment getLocalMealsFragment(){
+        if(localMeals.size() == 0){
+            for(int i = 0; i<20; i++){
+                Meal meal = new Meal();
+                meal.thumbnail = "http://img.recipepuppy.com/16.jpg";
+                meal.title = "TO BE REPLACED WITH SQLITE LOCAL MEALS";
+                meal.ingredients = "Dummy Chicken, Dummy Rice";
+                meal.href = "https://www.allrecipes.com/recipe/14746/mushroom-pork-chops/";
+                localMeals.add(meal);
+            }
         }
-
         //Create argument bundle that contains the meal list to display in the particular recyclerview
         Bundle args = new Bundle();
         args.putParcelableArrayList(MEALS__KEY, localMeals);
+        args.putBoolean("isTwoPane", isTwoPane);
         MealsFragment localListFragment = new MealsFragment();
         localListFragment.setArguments(args);
         return localListFragment;
