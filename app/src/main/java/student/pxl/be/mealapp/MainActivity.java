fa1a6e61485cb67d702908669f03a37d8e0426ac
DavidRandisi <david_randisi@hotmail.com>
2018-10-21 19:01:57 +0200

    Added navigation between categories in landscape mode

--- app/src/main/java/student/pxl/be/mealapp/MainActivity.java
+++ app/src/main/java/student/pxl/be/mealapp/MainActivity.java
@@ -1,23 +1,19 @@
 package student.pxl.be.mealapp;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import student.pxl.be.mealapp.domain.Meal;
@@ -29,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Meal> favoriteMeals;
     private ArrayList<Meal> localMeals;
     private boolean isTwoPane;
-    private ViewPager mviewPager;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,17 +36,18 @@ public class MainActivity extends AppCompatActivity {
         randomMeals = new ArrayList<>();
         favoriteMeals = new ArrayList<>();
         localMeals = new ArrayList<>();
-        mviewPager = findViewById(R.id.vp_id);
 
-        //TODO: add navigation in TwoPane (landscape) mode between explore/favorites/local
-        if(isTwoPane){
+        if (isTwoPane) {
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             //TODO: save CURRENT viewpager fragment instance to display it in landscape mode
             fragmentTransaction.replace(R.id.land_list_frame_id, getExploreMealsFragment());
             fragmentTransaction.commit();
+
+            setupMenuNavigation();
         } else {
             TabLayout mtabLayout = findViewById(R.id.tabs);
+            ViewPager mviewPager = findViewById(R.id.vp_id);
             ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter();
             mviewPager.setAdapter(viewPagerAdapter);
             mtabLayout.setupWithViewPager(mviewPager);
@@ -58,12 +55,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Decides if the screen is in twoPane mode by checking if the meal detail frame is available
-    private void determineTwoPane(){
-        if(findViewById(R.id.land_detail_frame_id) != null){
+    private void determineTwoPane() {
+        if (findViewById(R.id.land_detail_frame_id) != null) {
             isTwoPane = true;
         } else {
             isTwoPane = false;
         }
+    }
+
+    //TODO: Add burger menu icon in landscape mode to open up the navigationdrawer
+    private void setupMenuNavigation() {
+        mDrawerLayout = findViewById(R.id.land_drawer_layout_id);
+        NavigationView navigationView = findViewById(R.id.land_navigation_id);
+        navigationView.setNavigationItemSelectedListener((menuItem) -> {
+            // set item as selected to persist highlight
+            menuItem.setChecked(true);
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers();
+
+            // Swap MealsFragment based on clicked menu item
+            int id = menuItem.getItemId();
+            Fragment fragment = null;
+
+            switch(id){
+                case R.id.nav_explore_id:
+                    fragment = getExploreMealsFragment();
+                    break;
+                case R.id.nav_favorite_id:
+                    fragment = getFavoriteMealsFragment();
+                    break;
+                case R.id.nav_local_id:
+                    fragment = getLocalMealsFragment();
+                    break;
+            }
+            if(fragment != null){
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.land_list_frame_id, fragment);
+                fragmentTransaction.commit();
+            }
+
+            return true;
+        });
     }
 
     private class ViewPagerAdapter extends FragmentPagerAdapter {
@@ -75,11 +107,15 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public Fragment getItem(int position) {
             switch (position) {
-                case 0: return getExploreMealsFragment();
-                case 1: return getFavoriteMealsFragment();
-                case 2: return getLocalMealsFragment();
+                case 0:
+                    return getExploreMealsFragment();
+                case 1:
+                    return getFavoriteMealsFragment();
+                case 2:
+                    return getLocalMealsFragment();
 
-                default: throw new IllegalArgumentException("unexpected position: " + position);
+                default:
+                    throw new IllegalArgumentException("unexpected position: " + position);
             }
         }
 
@@ -87,11 +123,15 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
-                case 0: return "EXPLORE";
-                case 1: return "FAVORITES";
-                case 2: return "LOCAL";
+                case 0:
+                    return "EXPLORE";
+                case 1:
+                    return "FAVORITES";
+                case 2:
+                    return "LOCAL";
 
-                default: throw new IllegalArgumentException("unexpected position: " + position);
+                default:
+                    throw new IllegalArgumentException("unexpected position: " + position);
             }
         }
 
@@ -104,8 +144,8 @@ public class MainActivity extends AppCompatActivity {
     //TODO: replace dummy data with data from a AsyncTaskLoader API call
     //TODO: save fragment state so the API call doesn't get executed on every screen rotation
     private MealsFragment getExploreMealsFragment() {
-        if(randomMeals.size() == 0){
-            for(int i = 0; i<20; i++){
+        if (randomMeals.size() == 0) {
+            for (int i = 0; i < 20; i++) {
                 Meal meal = new Meal();
                 meal.thumbnail = "http://img.recipepuppy.com/11.jpg";
                 meal.title = "TO BE REPLACED WITH API MEALS";
@@ -125,9 +165,9 @@ public class MainActivity extends AppCompatActivity {
         return MealsFragment.newInstance(randomMeals, isTwoPane);
     }
 
-    private MealsFragment getFavoriteMealsFragment(){
-        if(favoriteMeals.size() == 0){
-            for(int i = 0; i<20; i++){
+    private MealsFragment getFavoriteMealsFragment() {
+        if (favoriteMeals.size() == 0) {
+            for (int i = 0; i < 20; i++) {
                 Meal meal = new Meal();
                 meal.thumbnail = "http://img.recipepuppy.com/12.jpg";
                 meal.title = "TO BE REPLACED WITH SQLITE FAVORITE MEALS";
@@ -140,9 +180,9 @@ public class MainActivity extends AppCompatActivity {
         return MealsFragment.newInstance(favoriteMeals, isTwoPane);
     }
 
-    private MealsFragment getLocalMealsFragment(){
-        if(localMeals.size() == 0){
-            for(int i = 0; i<20; i++){
+    private MealsFragment getLocalMealsFragment() {
+        if (localMeals.size() == 0) {
+            for (int i = 0; i < 20; i++) {
                 Meal meal = new Meal();
                 meal.thumbnail = "http://img.recipepuppy.com/16.jpg";
                 meal.title = "TO BE REPLACED WITH SQLITE LOCAL MEALS";
